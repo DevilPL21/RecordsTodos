4bd14322fa73b7cb5a40e6f1ddebbe8f63b8470d
DevilPL21 <137456892+DevilPL21@users.noreply.github.com>
2023-06-23 12:29:56 +0530

    Update SearchComponent.js

--- screens/SearchComponent.js
+++ screens/SearchComponent.js
@@ -7,12 +7,6 @@ const SearchComponent = ({records}) => {
     const [searchText, setSearchText] = useState('');
     const [filteredData, setFilteredData] = useState([]);
     const dispatch = useDispatch();
-    // Replace this dummy data with your actual data source
-    const data = [
-      { id: 1, name: 'John Doe', email: 'john@example.com' },
-      { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
-      // ... more data
-    ];
   
     const handleSearch = async (text) => {
       setSearchText(text);
@@ -23,8 +17,6 @@ const SearchComponent = ({records}) => {
           item.Name.toLowerCase().includes(text.toLowerCase()) ||
           item.Email.toLowerCase().includes(text.toLowerCase())
       );
-    //   console.log("oh yes", filtered)
-    //   setFilteredData(filtered);
     if(text.trim() === ""){
         const storedRecords = await AsyncStorage.getItem('records');
         dispatch({ type: 'SEARCH_RECORDS', payload: storedRecords ? JSON.parse(storedRecords) : []});
@@ -33,14 +25,6 @@ const SearchComponent = ({records}) => {
         dispatch({ type: 'SEARCH_RECORDS', payload: filtered});
     }
     };
-  
-    // const renderItem = ({ item }) => (
-    //   <View>
-    //     {/* Replace this with your actual item rendering */}
-    //     <Text>{item.Name}</Text>
-    //     <Text>{item.Email}</Text>
-    //   </View>
-    // );
   
     return (
       <View>
@@ -52,11 +36,6 @@ const SearchComponent = ({records}) => {
             style={styles.cardStyle}
           />
         </View>
-        {/* <FlatList
-          data={filteredData}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.key}
-        /> */}
       </View>
     );
   };
@@ -66,7 +45,6 @@ const SearchComponent = ({records}) => {
       borderWidth: 1,
       borderColor: "lightgrey",
       borderRadius: 5,
-    //   borderStyle: solid,
       marginLeft: 10
     },
   });
